3ac7824e503d08a6a77dec716316dbf744673341
shaoxiong_lan <shaoxionglan@gmail.com>
2018-10-20 18:18:37 -0600

    basic setting

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.2;
+contract Election {
+
+    // write candidate
+
+    //read candidate
+    string public candidate;
+
+    // constructor
+    function Election() public {
+        candidate = "Cadidate 1";
+    }
+
+}
