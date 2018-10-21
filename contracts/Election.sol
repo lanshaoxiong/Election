8dc8f018ef44bcf97b9c2d5c058fd1d1747b0edd
shaoxiong_lan <shaoxionglan@gmail.com>
2018-10-20 21:00:48 -0600

    set up client side

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,14 +1,26 @@
 pragma solidity ^0.4.2;
 contract Election {
+    // model candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    // write candidate
-
-    //read candidate
-    string public candidate;
+    // store candidate
+    // fetch candidate
+    mapping(uint => Candidate) public candidates;
+    // store candidate count
+    uint public candidatesCount;
 
     // constructor
     function Election() public {
-        candidate = "Cadidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    } 
 }
